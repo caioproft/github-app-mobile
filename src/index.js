208dfddbadc8295fcb16ef08646bba0227315de4
Caio Proft <caio_proft@hotmail.com>
2020-03-19 11:52:11 -0300

    :construction: Configurando StatusBar

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import 'react-native-gesture-handler';
 import './config/ReactotronConfig';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 import React from 'react';
 import Routes from './routes';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Routes />
+      <>
+        <StatusBar barStyle="ligth-content" backgroundColor="#7159c1" />
+        <Routes />
+      </>
     </NavigationContainer>
   );
 }
